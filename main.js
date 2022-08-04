f1c7785de84abe69cfd303869f5c915aeda2a6c0
KENI0 <chl0933@naver.com>
2022-08-04 23:03:33 +0900

    js-practice-3

--- main.js
+++ main.js
@@ -1,15 +1,54 @@
-//생성자 함수 연습 
-function User(first, last) {
-  this.firstName = first
-  this.lastName = last 
+
+// function User(first, last) {
+//   this.firstName = first 
+//   this.lastName = last 
+// }
+
+// User.prototype.gettingFullName = function() {
+//   return `${this.firstName} ${this.lastName}`
+// }
+
+// class User {
+//   constructor(first, last) {
+//     this.firstName = first
+//     this.lastName = last 
+//   }
+//   gettingFullName() {
+//     return `${this.firstName} ${this.lastName}`
+//   }
+// }
+
+
+
+// const Woo = new User('Woo young', 'Choi')
+// const Sally = new User('Sally', 'Jane')
+
+// console.log(Woo.gettingFullName())
+// console.log(Sally.gettingFullName())
+
+class Vehicle {
+  constructor(name, wheel) {
+    this.name = name
+    this.wheel = wheel
+  }
+}
+const myVehicle = new Vehicle('운송수단', 2)
+console.log(myVehicle)
+
+class Bicycle extends Vehicle {
+  constructor(name, wheel){
+    super(name, wheel)
+  }
+}
+const myBicycle = new Bicycle('자전거', 2)
+console.log(myBicycle)
+
+class Car extends Vehicle {
+  constructor(name, wheel, license) {
+    super(name, wheel)
+    this.license = license
+  }
 }
 
-User.prototype.getFullName = function() {
-  return `${this.firstName} ${this.lastName}`
-}
-
-const Heize = new User('Heize','Sally') 
-const Tom = new User('Tom','Bradely') 
-
-console.log(Heize.getFullName())
-console.log(Tom)
+const myCar = new Car('벤츠', 4, true)
+console.log(myCar)
